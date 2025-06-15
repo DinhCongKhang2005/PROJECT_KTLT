3d178881b9c7b5cc669876b7865cf35659057dd0
Dinh Cong Khang <khang@gmail.com>
2025-06-15 22:35:35 +0700

    nfhic ngu

---
+++ hash.h
@@ -0,0 +1,8 @@
+#ifndef HASH_H
+#define HASH_H
+
+unsigned int hash(char *str); // Hàm băm cho khoá
+int compareString(void *a, void *b); // Hàm so sánh xâu
+
+// nghicx nfgsdifsdofjdsjflkds
+#endif
